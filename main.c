4f59af98b93ab5fef4fed58b5744b9a5eb1758d6
stivemungai <91313883+stivemungai@users.noreply.github.com>
2021-10-15 11:16:36 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+project:county library management system
+Author: stephen mungai
+Date: october 15
+Compile:GNU GCC
+Language: C99
+Licence:MIT
+*/
+int main()
+
+
+
+{
+    printf("programming is fun! \n");
+
+    return 0;
+}
